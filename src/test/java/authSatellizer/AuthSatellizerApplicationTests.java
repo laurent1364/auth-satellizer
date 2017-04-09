d9ca3b90a5f9ce043cf7127c0ba747044606ea5f
laurent1364 <laurent.faivre.64@gmail.com>
2017-04-09 15:55:32 +1200

    test unuse

---
+++ src/test/java/authSatellizer/AuthSatellizerApplicationTests.java
@@ -0,0 +1,16 @@
+package authSatellizer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AuthSatellizerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
